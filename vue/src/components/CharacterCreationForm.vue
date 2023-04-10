d89f9ae576aa8cee308b89849994578f2c4424ad
Milton Krauss <miltonkrauss@gmail.com>
2023-04-10 17:45:59 -0400

    much work on character creation form component

--- vue/src/components/CharacterCreationForm.vue
+++ vue/src/components/CharacterCreationForm.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="addHomeform">
-    <h1>Character Creator</h1>
+    <div id="title">
+      <h1 >Character Creator</h1>
+    </div>
     <!-- <p class="status-message error" v-show="errorMessage != ''">{{errorMessage}}</p> -->
     <form class="homeForm" v-on:submit.prevent="submitForm">
-      <div class="form-group">
-        <label for="name" >Name</label>
+      <div class="name-group" id="name-box">
+        <label for="name">Name</label>
         <input
           id="name"
           type="text"
@@ -12,49 +14,79 @@
           v-model="newCharacter.name"
         />
       </div>
-      <div id="race-class">
-      <div class="form-group">
-        <label for="race" >Race</label>
-        <input
-          id="race"
-          type="text"
-          class="form-control"
-          v-model="newCharacter.race"
-        />
-      </div>
-      
-      <div class="form-group">
-        <label for="charClass"  >Class</label>
-        <input
-          id="charClass"
-          type="text"
-          class="form-control"
-          v-model="newCharacter.charClass"
-        />
-      </div>
-      </div>
 
-      <div id="stats-picture">
-        <div id="stats-left">
-          <div id="str"><p>STRENGTH</p>5</div>
-          <div id="dex"><p>DEXTERITY</p>5</div>
-          <div id="con"><p>CONSTITUTION</p>5</div>
-
+      <!-- <div id="race-class"> -->
+        <div class="race-class-group" id="race">
+          <label for="race">Race</label>
+          <select
+            id="race-selection"
+            class="form-control"
+            >
+            <option 
+              v-for="race in dropdownRace" 
+              v-bind:key="race.id"
+              v-bind:value="race.name"
+            > {{race.name}}
+            </option>
+          </select>
+            
+            <!-- v-model="newCharacter.race" -->
         </div>
 
-        <div id="char-pic">
-         PICTURE PLACEHOLDER
+        <div class="race-class-group" id="class">
+          <label for="charClass">Class</label>
+          <select
+            id="class-selection"
+            class="form-control"
+            >
+            <option
+              v-for="charClass in dropdownClass"
+              v-bind:key="charClass.id"
+              v-bind:value="charClass.name"
+              > {{charClass.name}}
+            </option>
+          </select>
+        </div>
+      <!-- </div> -->
+
+      <!-- <div class="stats-picture"> -->
+        <div class="stats-column" id="lcol">
+          <div class="stats-box">
+            <div class="stats-name">STRENGTH</div>
+            <div class="stats-value" id="str-value">5</div>
+          </div>
+          <div class="stats-box">
+            <div class="stats-name">DEXTERITY</div>
+            <div class="stats-value" id="dex-value">5</div>
+          </div>
+          <div class="stats-box">
+            <div class="stats-name">CONSTITUTION</div>
+            <div class="stats-value" id="con-value">5</div>
+          </div>
         </div>
 
-        <div id="stats-right">
-          <div id="int"><p>INTELLIGENCE</p>5</div>
-          <div id="wis"><p>WISDOM</p>5</div>
-          <div id="cha"><p>CHARISMA</p>5</div>
+        <div id="picture">
+          <img src="https://cdna.artstation.com/p/assets/images/images/010/077/400/large/maja-weber-sun-wukong-colour.jpg?1522433552" class="char-pic" id="char-pic">
         </div>
-      </div>
+        
+        <div class="stats-column" id="rcol">
+          <div class="stats-box">
+            <div class="stats-name">INTELLIGENCE</div>
+            <div class="stats-value" id="int-value">5</div>
+          </div>
+          <div class="stats-box">
+            <div class="stats-name">WISDOM</div>
+            <div class="stats-value" id="wis-value">5</div>
+          </div>
+          <div class="stats-box">
+            <div class="stats-name">CHARISMA</div>
+            <div class="stats-value" id="int-value">5</div>
+          </div>
+        </div>
+      <!-- </div> -->
 
-      <div class="form-group">
-        <label for="description">Description:</label>
+      <div id="desc">
+        <label for="description">Description</label>
         <input
           id="description"
           type="textarea"
@@ -62,16 +94,18 @@
           v-model="newCharacter.description"
         />
       </div>
-
-      <button class="btn btn-submit">Submit</button>
-      <button class="btn btn-cancel" type="cancel" v-on:click="cancelForm">
-        Cancel
-      </button>
+      <div class="buttons" id="buttons">
+        <button class="btn btn-submit">Submit</button>
+        <button class="btn btn-cancel" type="cancel" v-on:click="cancelForm">
+          Cancel
+        </button>
+      </div>
     </form>
   </div>
 </template>
 
 <script>
+// import CharacterService from '../services/CharacterService.js'
 export default {
   name: "character-creation-form",
   data() {
@@ -90,6 +124,25 @@ export default {
           cha: 1,
         },
       },
+      dropdownRace: [
+        {
+          name: 'Dwarf'
+        },
+        {
+          name: 'Elf'
+        },
+        {
+          name: 'Halfling'
+        }
+      ],
+      dropdownClass: [
+        {
+          name: 'Barbarian'
+        },
+        {
+          name: 'Bard'
+        }
+      ]
     };
   },
   methods: {
@@ -110,53 +163,53 @@ export default {
       };
     },
     submitForm() {},
-    cancelForm() {},
+    cancelForm() {}
   },
 };
 </script>
 
 <style scoped>
-h1 {
-  text-align: center;
-}
-#name {
-  width: 50%;
-  text-align: center;
-  margin: 0 auto;
-  
-}
-
-#race-class {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  text-align: center;
-}
-
 #addHomeform {
-  margin-left: auto;
-  margin-right: auto;
+  
+  margin: 0 auto;
   width: 80%;
   background-color: lightgray;
+  padding: 1% 2%;
+}
+
+#title {
+ grid-area: h1; 
+}
+#title h1 {
+  text-align: center;
+  font-size: 4rem;
   
 }
-form input {
-  width: 100%;
-}
+
 .homeForm {
   padding: 10px;
   margin-bottom: 10px;
+  display: grid;
+  grid-template-columns: 1fr 10fr 1fr;
+  grid-template-areas:  "title    title     title"
+                        ".        name-box      ."
+                        "race     picture   class"
+                        "lcol     picture   rcol"
+                        "desc     desc      desc"
+                        ".        buttons   ."
+                        ;
+  gap: 10px;
 }
-.form-group {
+
+.name-group {
   margin: 10px 0;
   text-align: center;
   color: #38b412;
-
+  font-size: 2rem;
+  
 }
+
 .form-control {
-  display: flex;
-  align-items: flex-start;
-  /* justify-self: center; */
   width: 90%;
   height: 30px;
   padding: 0.375rem 0.75rem;
@@ -167,19 +220,117 @@ form input {
   border: 1px solid #ced4da;
   border-radius: 0.25rem;
 }
+
+#name-box {
+  /* width: 50%; */
+  margin: 0 auto;
+  grid-area: name-box;
+}
+
+#race-class {
+  text-align: center;
+}
+
+#race {
+  grid-area: race;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #38b412;
+  font-size: 2rem;
+}
+
+#race label {
+
+}
+
+#picture {
+  grid-area: picture;
+}
+
+#class {
+  grid-area: class;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #38b412;
+  font-size: 2rem;
+}
+
+#lcol {
+  grid-area: lcol;
+}
+
+#rcol {
+  grid-area: rcol;
+}
+
+#desc {
+  grid-area: desc;
+  margin: 10px 0;
+  text-align: center;
+  color: #38b412;
+  font-size: 2rem;
+}
+
+#buttons {
+  grid-area: buttons;
+  margin: auto;
+}
+
 textarea.form-control {
   height: 75px;
   font-family: Arial, Helvetica, sans-serif;
 }
+
 select.form-control {
   width: 20%;
   display: inline-block;
   margin: 10px 20px 10px 10px;
 }
+
+.stats-picture {
+  margin: 2rem 0;
+}
+
+#char-pic {
+  background-color: aliceblue;
+  max-width: 100%;
+  height: auto;
+  margin: 50px 0 0 0;
+}
+
+.stats-column {
+  background-color: bisque;
+  border-radius: 3px;
+  padding: 1rem;
+  box-shadow: 0 12px 26px 0 rgba(0, 0, 0, 0.24),
+    0 17px 50px 0 rgba(0, 0, 0, 0.19);
+}
+
+.stats-box {
+  text-align: center;
+  margin: 1rem;
+}
+
+.stats-name {
+  font-size: 1.2rem;
+  font-weight: 700;
+}
+
+.stats-value {
+  font-size: 2rem;
+  background-color: rgb(252, 247, 240);
+  width: 50%;
+  margin: 0 auto;
+  border-radius: 6px;
+}
+
 button {
   border-radius: 6px;
   margin: 0 10px 0 0;
 }
+
 .btn-submit {
   color: #fff;
   padding: 10px 24px;
@@ -219,12 +370,6 @@ button {
 }
 .status-message.error {
   background-color: #f08080;
-}
-#stats-picture {
-  display:flex;
-  flex-direction: row;
-  justify-content: space-between;
-
 }
 
 
