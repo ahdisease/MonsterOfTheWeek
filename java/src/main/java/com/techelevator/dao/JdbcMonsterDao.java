4de4c89de853e8fd98a965b030caf920ecdc1235
Kayla Spano <kspan13@aol.com>
2023-04-10 14:44:59 -0400

    monster tables/dao

---
+++ java/src/main/java/com/techelevator/dao/JdbcMonsterDao.java
@@ -0,0 +1,25 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Monster;
+
+import java.time.LocalDate;
+
+public class JdbcMonsterDao implements MonsterDao{
+
+
+    @Override
+    public Monster getMonsterByDate(LocalDate date) {
+
+
+        return null;
+    }
+
+    @Override
+    public Monster addMonster() {
+
+        
+        return null;
+    }
+
+
+}
---
+++ java/src/main/java/com/techelevator/dao/MonsterDao.java
@@ -0,0 +1,14 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Monster;
+
+import java.time.LocalDate;
+
+public interface MonsterDao {
+
+    Monster getMonsterByDate(LocalDate date);
+
+    Monster addMonster();
+
+
+}
