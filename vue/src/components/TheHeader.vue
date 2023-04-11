dfa6768a44b4a75a23e5ebbca367df59dcd144ad
Milton Krauss <miltonkrauss@gmail.com>
2023-04-11 11:49:46 -0400

    pulling

--- vue/src/components/TheHeader.vue
+++ vue/src/components/TheHeader.vue
@@ -2,9 +2,8 @@
   <div>
     <h2>Monster of the Week LOGOHERE</h2>
     <nav id="nav">
-        <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-       <router-link v-bind:to="{ name: 'character-creator' }">Character Creator</router-link>&nbsp;|&nbsp;
-
+      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'character-creator' }">Character Creator</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </nav>
   </div>
