3bb92d8d775663939b358f7e750aa0268a4ce41b
Kayla Spano <kspan13@aol.com>
2023-04-12 14:39:06 -0400

    party vote vue

---
+++ vue/src/views/PartyRankings.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      
+    </div>
+</template>
+
+<script>
+import PartyComponent from "../components/PartyComponent.vue"
+import CharacterService from "../services/CharacterService.js";
+
+export default {
+    name: 'party-rankings',
+    parties: [],
+
+    components: {
+        PartyComponent
+    },
+
+    created() {
+        CharacterService.getAllParties().then((response) => {
+            this.parties.push(response);
+        })
+    },
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
