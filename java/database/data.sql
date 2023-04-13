405a2fd61bbc8d85168e74f1f9664eec07563ffb
Alexander Stuart <ahdisease@gmail.com>
2023-04-13 13:04:30 -0400

    added backed ability to flag characters

--- java/database/data.sql
+++ java/database/data.sql
@@ -1,34 +1,50 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER'); --id 1
+INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN'); --id 2
+INSERT INTO users (username,password_hash,role) VALUES ('mod','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_MOD'); --id 3
+INSERT INTO users (username,password_hash,role) VALUES ('a','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER'); --id 4
+INSERT INTO users (username,password_hash,role) VALUES ('b','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER'); --id 5
+INSERT INTO users (username,password_hash,role) VALUES ('c','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER'); --id 6
+INSERT INTO users (username,password_hash,role) VALUES ('d','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER'); --id 7
+INSERT INTO users (username,password_hash,role) VALUES ('e','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER'); --id 8
+INSERT INTO users (username,password_hash,role) VALUES ('f','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER'); --id 9
 
-INSERT INTO monster (name_index, start_date, end_date) VALUES ('monster','2020-01-01','2020-01-07');
-INSERT INTO monster (name_index, start_date, end_date) VALUES ('monstar','2020-01-08','2020-01-14');
-INSERT INTO monster (name_index, start_date, end_date) VALUES ('aboleth','2023-04-09','2023-04-15');
+INSERT INTO monster (name_index, start_date, end_date) VALUES ('monster','2020-01-01','2020-01-07'); --id 1
+INSERT INTO monster (name_index, start_date, end_date) VALUES ('monstar','2020-01-08','2020-01-14'); --id 2
+INSERT INTO monster (name_index, start_date, end_date) VALUES ('aboleth','2023-04-09','2023-04-15'); --id 3
+INSERT INTO monster (name_index, start_date, end_date) VALUES ('adult-black-dragon','2023-04-16','2023-04-22'); --id 4
+
 
 INSERT INTO character (name, char_class, race, description, strength, dexterity, constitution, intelligence, wisdom, charisma, monster_id, user_id)
-	VALUES ('Jet', 'Fighter', 'Elf', 'Swims and fights', 10,10,10,10,10,10, 3,1);
+	VALUES ('Jet', 'Fighter', 'Elf', 'Swims and fights', 18,17,15,12,13,14, 3,1); --id 1
 INSERT INTO character (name, char_class, race, description, strength, dexterity, constitution, intelligence, wisdom, charisma, monster_id, user_id)
-	VALUES ('Ba', 'Wizard', 'Dwarf', 'Tamed a basalisk once', 10,10,10,10,10,10, 3,2);
+	VALUES ('Ba', 'Wizard', 'Dwarf', 'Tamed a basalisk once', 14,18,16,15,12,13, 3,2); --id 2
 INSERT INTO character (name, char_class, race, description, strength, dexterity, constitution, intelligence, wisdom, charisma, monster_id, user_id)
-	VALUES ('Lyfet', 'Rouge', 'Elf', 'Do not trust him', 10,10,10,10,10,10, 3,1);
+	VALUES ('Lyfet', 'Rouge', 'Elf', 'Do not trust him', 13,14,18,16,15,12, 3,3); --id 3
 INSERT INTO character (name, char_class, race, description, strength, dexterity, constitution, intelligence, wisdom, charisma, monster_id, user_id)
-	VALUES ('Keen', 'Fighter', 'Dragon Born', 'Friendly but scary', 10,10,10,10,10,10, 3,2);
+	VALUES ('Keen', 'Fighter', 'Dragon Born', 'Friendly but scary', 12,13,14,18,16,15, 3,4); --id 4
 INSERT INTO character (name, char_class, race, description, strength, dexterity, constitution, intelligence, wisdom, charisma, monster_id, user_id)
-	VALUES ('Teg', 'Fighter', 'Elf', 'Floats', 10,10,10,10,10,10, 3,2);
+	VALUES ('Teg', 'Fighter', 'Elf', 'Floats', 15,12,13,14,18,16, 3,5); --id 5
 INSERT INTO character (name, char_class, race, description, strength, dexterity, constitution, intelligence, wisdom, charisma, monster_id, user_id)
-	VALUES ('Zic', 'Fighter', 'Dwarf', 'Unicorn Blood', 10,10,10,10,10,10, 3,1);
+	VALUES ('Zic', 'Fighter', 'Dwarf', 'Unicorn Blood', 16,15,12,13,14,18, 3,6); --id 6
 INSERT INTO character (name, char_class, race, description, strength, dexterity, constitution, intelligence, wisdom, charisma, monster_id, user_id)
-	VALUES ('Fig', 'Fighter', 'Elf', 'Loves eating', 10,10,10,10,10,10, 3,2);
+	VALUES ('Fig', 'Fighter', 'Elf', 'Loves eating', 10,10,10,10,10,10, 3,7); --id 7
+INSERT INTO character (name, char_class, race, description, strength, dexterity, constitution, intelligence, wisdom, charisma, monster_id, user_id)
+	VALUES ('Toblerone', 'Rogue', 'Dwarf', 'Builds machines to fight for him', 10,10,10,10,10,10, 3,8); --id 8
+INSERT INTO character (name, char_class, race, description, strength, dexterity, constitution, intelligence, wisdom, charisma, monster_id, user_id)
+	VALUES ('Reinhardt', 'Monk', 'Elf', 'He stuns people.', 10,10,10,10,10,10, 3,9); --id 9
 	
-INSERT INTO character (name, char_class, race, description, strength, dexterity, constitution, intelligence, wisdom, charisma, monster_id, user_id)
-	VALUES ('Toblerone', 'Rogue', 'Dwarf', 'Builds machines to fight for him', 10,10,10,10,10,10, 3,1);
-INSERT INTO character (name, char_class, race, description, strength, dexterity, constitution, intelligence, wisdom, charisma, monster_id, user_id)
-	VALUES ('Reinhardt', 'Monk', 'Elf', 'He stuns people.', 10,10,10,10,10,10, 3,2);
-	
-INSERT INTO party (character_1, character_2, character_3, character_4) VALUES (1,2,3,4);
+INSERT INTO party (character_1, character_2, character_3, character_4) VALUES (1,2,3,4); --id 1 monster 3
+INSERT INTO party (character_1, character_2, character_3, character_4) VALUES (2,4,5,7); --id 2 monster 3
+INSERT INTO party (character_1, character_2, character_3, character_4) VALUES (3,4,5,7); --id 2 monster 3
 
-INSERT INTO users_party (user_id,party_id) VALUES (1,1);
+
+INSERT INTO users_party (user_id,party_id) VALUES (4,1); --monster 3
+INSERT INTO users_party (user_id,party_id) VALUES (5,2); --monster 3
+INSERT INTO users_party (user_id,party_id) VALUES (6,1); --monster 3
+INSERT INTO users_party (user_id,party_id) VALUES (7,3); --monster 3
+INSERT INTO users_party (user_id,party_id) VALUES (8,3); --monster 3
+INSERT INTO users_party (user_id,party_id) VALUES (9,1); --monster 3
 
 COMMIT TRANSACTION;
