73162581c27cf0a094dd3550093077eebc777bd5
Benjamin Grover <benmgrover@gmail.com>
2023-04-13 20:27:55 -0400

    backing up the party compnonent and home view

--- vue/src/components/PartyWinner.vue
+++ vue/src/components/PartyWinner.vue
@@ -5,7 +5,11 @@
     </div>
 
     <div class="party-display">
-      <div class="the-cards" v-for="character in winningParty" v-bind:key="character.id700">
+      <div
+        class="the-cards"
+        v-for="character in winningParty"
+        v-bind:key="character.id700"
+      >
         <character-card v-bind:character="character">
           {{ character.race }}
           {{ character.charClass }}
@@ -37,7 +41,28 @@ export default {
   created() {
     CharacterService.getWinningParty("2023-04-09").then(
       (response) => {
-        this.winningParty = response.data;
+        if (response.data) {
+          
+          const partyObject = response.data;
+          CharacterService.getCharacterById(partyObject.characterOne).then(
+            (response) => {
+              this.currentParty.push(response.data);
+            }
+          );
+          CharacterService.getCharacterById(partyObject.characterTwo).then(
+            (response) => {
+              this.currentParty.push(response.data);
+            }
+          );
+          CharacterService.getCharacterById(partyObject.characterThree).then(
+            (response) => {
+              this.currentParty.push(response.data);
+            }
+          );
+          CharacterService.getCharacterById(partyObject.characterFour).then(
+            (response) => {
+              this.currentParty.push(response.data);
+            })}
       }
     );
   },
@@ -46,38 +71,35 @@ export default {
 
 <style scoped>
 #wrapper {
-  border: solid linear-gradient( rgb(0, 0, 0), rgba(255,0,0,0));
-  border-width: 10px 1px 1px 0;
+  border: 10px solid linear-gradient(rgb(0, 0, 0), rgba(255, 0, 0, 0));
+  /* border-width: 10px 1px 1px 0; */
   border-radius: 6px;
   width: 100%;
-  background-image: linear-gradient( rgba(0, 60, 255, 0.945), rgba(255,0,0,0));
-
+  background-image: linear-gradient(
+    rgba(0, 60, 255, 0.945),
+    rgba(255, 0, 0, 0)
+  );
 }
 #winner-title {
-    padding: 10px;
-    height: 50px;
+  padding: 10px;
+  height: 50px;
   /* background-image: linear-gradient( rgba(255,0,0,1), rgba(255,0,0,0)); */
-    /* This is a temporary color to see where it is */
+  /* This is a temporary color to see where it is */
 }
 #winner-title h3 {
-    color: whitesmoke;
-    text-align: center;
+  color: whitesmoke;
+  text-align: center;
 }
-.party-display{
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-evenly;
-    
+.party-display {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
 }
 
-
-
-
-
-
-
-
-
-
+@media screen and (max-width: 1000px) {
+  /* #wrapper {
+    width: 50%;
+  } */
+}
 </style>
