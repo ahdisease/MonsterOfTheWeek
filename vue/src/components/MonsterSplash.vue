96d62fb4fca3cc919fb1ad268c0fb7189a00a6e2
Benjamin Grover <benmgrover@gmail.com>
2023-04-15 08:56:46 -0400

    worked on sizing of monster splash comp and home view

--- vue/src/components/MonsterSplash.vue
+++ vue/src/components/MonsterSplash.vue
@@ -1,50 +1,64 @@
 <template>
   <main>
-    <div
-      class="monster-card-front"
-      v-show="!showDetails"
-      v-on:click="turnMonsterCard">
-      <img
-        v-bind:src="'https://www.dnd5eapi.co' + monster.image"
-        alt="image of monster"
-      />
-    </div>
-    <div
-      class="monster-card-back"
-      v-show="showDetails"
-      v-on:click="turnMonsterCard"
-    >
-      <h3>{{ monster.name }}</h3>
-      <div class="hit-points">HP: {{ monster.hit_points }}</div>
-      <div class="all-stats">
-        <div class="stats-box">
-          <div class="stats-name">STRENGTH</div>
-          <div class="stats-value" id="str-value"> {{monster.strength}} </div>
-        </div>
-        <div class="stats-box">
-          <div class="stats-name">DEXTERITY</div>
-          <div class="stats-value" id="dex-value"> {{monster.dexterity}} </div>
-        </div>
-        <div class="stats-box">
-          <div class="stats-name">CONSTITUTION</div>
-          <div class="stats-value" id="con-value"> {{monster.constitution}} </div>
-        </div>
-        <div class="stats-box">
-          <div class="stats-name">INTELLIGENCE</div>
-          <div class="stats-value" id="int-value"> {{monster.intelligence}} </div>
-        </div>
-        <div class="stats-box">
-          <div class="stats-name">WISDOM</div>
-          <div class="stats-value" id="wis-value"> {{monster.wisdom}} </div>
-        </div>
-        <div class="stats-box">
-          <div class="stats-name">CHARISMA</div>
-          <div class="stats-value" id="cha-value"> {{monster.charisma}} </div>
+    <!-- The code below this works **** -->
+    <div class="container">
+      <h1 id="monsterName">{{ monster.name }}</h1>
+
+      <div
+        class="monster-card-front"
+        v-show="!showDetails"
+        v-on:click="turnMonsterCard"
+      >
+        <img
+          v-bind:src="'https://www.dnd5eapi.co' + monster.image"
+          alt="image of monster"
+        />
+      </div>
+      <!-- The code above this works ***** -->
+
+      <!-- Below this is what works -->
+      <div
+        class="monster-card-back"
+        v-show="showDetails"
+        v-on:click="turnMonsterCard"
+      >
+        <h3>{{ monster.name }}</h3>
+        <div class="hit-points">HP: {{ monster.hit_points }}</div>
+        <div class="all-stats">
+          <div class="stats-box">
+            <div class="stats-name">STRENGTH</div>
+            <div class="stats-value" id="str-value">{{ monster.strength }}</div>
+          </div>
+          <div class="stats-box">
+            <div class="stats-name">DEXTERITY</div>
+            <div class="stats-value" id="dex-value">
+              {{ monster.dexterity }}
+            </div>
+          </div>
+          <div class="stats-box">
+            <div class="stats-name">CONSTITUTION</div>
+            <div class="stats-value" id="con-value">
+              {{ monster.constitution }}
+            </div>
+          </div>
+          <div class="stats-box">
+            <div class="stats-name">INTELLIGENCE</div>
+            <div class="stats-value" id="int-value">
+              {{ monster.intelligence }}
+            </div>
+          </div>
+          <div class="stats-box">
+            <div class="stats-name">WISDOM</div>
+            <div class="stats-value" id="wis-value">{{ monster.wisdom }}</div>
+          </div>
+          <div class="stats-box">
+            <div class="stats-name">CHARISMA</div>
+            <div class="stats-value" id="cha-value">{{ monster.charisma }}</div>
+          </div>
         </div>
       </div>
+      <!-- This is the end of the div that works *** -->
     </div>
-
-
   </main>
 </template>
 
@@ -75,6 +89,16 @@ export default {
     turnMonsterCard() {
       this.showDetails = !this.showDetails;
     },
+    // this is a testing method
+    // openMonsterModal() {
+    //   // this shows the modal
+    //   this.$refs["myMonster"].show();
+    // },
+    // closeMonsterModal() {
+    //   // this closes the modal
+    //   this.$refs["myMonster"].hide();
+    // },
+    // this ends the testing method
   },
 };
 </script>
@@ -82,48 +106,62 @@ export default {
 <style scoped>
 body {
   background: #096660;
-
 }
 
-.h3 {
+h3 {
   text-align: center;
 }
 
-.hit-points {
-  
-}
-
-main {
+/* main {
   margin: 5%;
+} */
+#monsterName {
+  font-size: 3em;
+  text-align: center;
 }
 
 .monster-card-front,
 .monster-card-back {
-  
   width: 100%;
   height: auto;
   border-radius: 10px;
-  background-color: grey;
-  
+  background-color: #c52b1e;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+
+.monster-card-back h3 {
+  padding: 10px;
+  font-size: 2.5em;
+}
+.hit-points {
+  font-weight: bold;
+  font-size: 1.25em;
+  /* padding: 5px; */
+}
+.all-stats {
+  display: flex;
   flex-wrap: wrap;
+  padding: 10px;
+  gap: 10px;
+  /* justify-content:space-around; */
+  align-content: space-around;
 }
 
 .stats-box {
   text-align: center;
-  /*TODO ******** check this one again */
-  /* margin: 0.5em; */
+  flex: 30%;
 }
 
 .stats-name {
-  font-size: 1.2rem;
-  font-weight: 700;
+  font-size: 1em;
+  font-weight: 500;
 }
 
 .stats-value {
-  font-size: 2em;
+  font-size: 1em;
   background-color: rgb(252, 247, 240);
   width: 50%;
   margin: 0 auto;
@@ -135,4 +173,17 @@ main {
   max-height: auto;
   border-radius: 10px;
 }
+
+/* 
+@media screen and (max-width: 1000px) {
+  
+
+  .stats-name {
+  font-size: .9em;
+  font-weight: 500;
+}
+
+
+
+} */
 </style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="home">
-    <div class="monster-space">
-      <monster-splash />
-    </div>
-    <div class="versus">VS</div>
-    <div class="party-space">
-      <party-component />
+    <p id="intro">
+      Welcome to Monster of the Week, where you pick your party to battle
+      against a monster every week. Start by
+      <b-btn class="callToAction">          <router-link class="nav-options" v-bind:to="{ name: 'character-view' }">Creating a Character</router-link>
+ </b-btn>
+      or go straight to <b-btn class="callToAction">      <router-link class="nav-options" v-bind:to="{ name: 'party' }">Forming a Party</router-link>
+ </b-btn>
+    </p>
+    <div id="main-battle">
+      <div class="monster-space">
+        <monster-splash />
+      </div>
+      <div class="versus"><h1>VS</h1></div>
+      <div class="party-space">
+        <party-component />
+      </div>
     </div>
     <div id="winner-space">
       <party-winner />
@@ -14,61 +24,90 @@
 </template>
 
 <script>
-import MonsterSplash from '../components/MonsterSplash.vue'
-import PartyComponent from '../components/PartyComponent.vue'
-import PartyWinner from '../components/PartyWinner.vue';
-
-
+import MonsterSplash from "../components/MonsterSplash.vue";
+import PartyComponent from "../components/PartyComponent.vue";
+import PartyWinner from "../components/PartyWinner.vue";
 
 export default {
   name: "home",
   components: {
     MonsterSplash,
     PartyComponent,
-    PartyWinner
-
-  }
+    PartyWinner,
+  },
 };
 </script>
 
 <style scoped>
-
-body {
-    display: grid;
-    background: #096660;
-    grid-template-areas: "home"
-                         "monster-space"
-                         "versus"
-                         "party-space"
-                         "winner-space";
-    margin: 0;
-    padding: 0;
-    grid-template-rows: 1fr 1fr 4fr 4fr 1fr;
-    
-}
+/* body {
+  display: grid;
+  background: #096660;
+  grid-template-areas:
+    "home"
+    "monster-space"
+    "versus"
+    "party-space"
+    "winner-space";
+  margin: 0;
+  padding: 0;
+  grid-template-rows: 1fr 1fr 4fr 4fr 1fr;
+} */
 
 .home {
   grid-area: home;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  /* justify-content: space-between; */
   align-items: center;
-  
+}
+#intro{
+  text-align: center;
+  font-weight: 500;
+  padding: 10px 20px;
+}
+.callToAction {
+  padding: 0px 4px;
+}
+.nav-options {
+  color: white;
+  text-decoration: none;
+}
+.nav-options a:hover {
+  /* color: rgb(180, 131, 39); */
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+}
+#main-battle {
+  display: flex;
+  flex-direction: row;
 }
 
+
+
 .monster-space {
-  width: auto;
+  flex: 60%;
   grid-area: monster-space;
 }
 
 .versus {
+  /* align-self: center; */
   grid-area: versus;
-  font-size: 3rem;
+  /* font-size: 3rem;
   text-align: center;
-  color: #00E88A;
+  color: #00e88a;
+  -webkit-text-stroke-width: 0.5px;
+  -webkit-text-stroke-color: black;
+  text-shadow: 2px 2px 6px rgb(54, 2, 2); */
+}
+.versus h1 {
+
+  font-size: 5em;
+  text-align: center;
+  color: #00e88a;
   -webkit-text-stroke-width: 0.5px;
   -webkit-text-stroke-color: black;
   text-shadow: 2px 2px 6px rgb(54, 2, 2);
+
 }
 
 .party-space {
@@ -82,23 +121,16 @@ body {
   grid-area: winner-space;
 }
 
+@media screen and (max-width: 768px) {
+  /* putting this in so it's a remind and so it's not empty  */
 
+  #main-battle {
+    flex-direction: column;
+  }
 
-
-@media screen and (max-width: 1000px) {
-  
-/* putting this in so it's a remind and so it's not empty  */
-
-#winner-space {
-  width: 75%;
-  margin: 10px 0;
+  #winner-space {
+    width: 75%;
+    margin: 10px 0;
+  }
 }
-
-
-
-
-
-}
-
-
 </style>
