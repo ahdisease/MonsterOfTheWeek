49a1680feaae74c75192acd08d6ea226adbb0bb8
Alexander Stuart <ahdisease@gmail.com>
2023-04-15 10:48:32 -0400

    Added Cloudinary Image component

---
+++ vue/src/components/CloudinaryImage.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <AdvancedImage v-bind:cldImg="myImg" />
+</template>
+
+<script>
+import { AdvancedImage } from '@cloudinary/vue'
+import { Cloudinary } from '@cloudinary/url-gen'
+export default {
+  components: {
+    AdvancedImage
+  },
+  data() {
+    return {
+      cloud: {},
+      myImg: {}
+    }
+  },
+  created() {
+    this.cloud = new Cloudinary({
+      cloud: {
+        cloudName: 'c19-lima-monster-of-the-week',
+        apiKey: '965275756759159'
+      }
+    })
+
+    this.myImg = this.cloud.image('sample')
+  }
+}
+</script>
+
+
+<style scoped>
+
+img {
+    width: 220px;
+}
+</style>
