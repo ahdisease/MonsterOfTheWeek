f27fbb10e484f5918a29ea869cf5d81fa1155942
Benjamin Grover <benmgrover@gmail.com>
2023-04-18 22:38:24 -0400

    backing up splash page comp

--- vue/src/components/SplashPage.vue
+++ vue/src/components/SplashPage.vue
@@ -1,13 +1,13 @@
 <template>
-  <div id="wrapper" class="bg-image">
+  <div id="wrapper">
     <main id="main-splash">
       <h1>Battle the Monster of the Week!</h1>
       <p></p>
       <h2>Create your very own character to adventure with!</h2>
-      <b-button><router-link class="nav-options" v-bind:to="{ name: 'character-view' }"
+      <b-button class="cta"><router-link class="nav-options" v-bind:to="{ name: 'character-view' }"
           >CREATE YOUR CHARACTER</router-link></b-button>
       <h2>Join with other characters and form a party to face the monster!</h2>
-      <b-button><router-link class="nav-options" v-bind:to="{ name: 'party' }"
+      <b-button class="cta"><router-link class="nav-options" v-bind:to="{ name: 'party' }"
           >ASSEMBLE YOUR PARTY</router-link></b-button>
       <p>
         The combination of characters that gets picked the most is the winning
@@ -26,18 +26,32 @@ export default {};
 #wrapper {
   background-color: #3a5268;
   width: 100%;
+  background-image: url("https://res.cloudinary.com/c19-lima-monster-of-the-week/image/upload/v1681865026/SCAG_1920x1080_Wallpaper_cmmsj8.jpg");
+  background-size: contain;
+  background-repeat: no-repeat;
+  min-height: 725px;
+  background-position-x: center;
+  background-position-y:top;
 
 }
-.bg-image {
-    background-image: url("https://res.cloudinary.com/c19-lima-monster-of-the-week/image/upload/v1681865026/SCAG_1920x1080_Wallpaper_cmmsj8.jpg");
-  width: 100%;
-}
+
 #main-splash {
-  width: 80%;
+  width: 70%;
   margin: 0 auto;
+  padding: 30px; 
+  /* color: #00e88a; */
+  color: white;
+  background-color: #ffffff15;
+  text-shadow: 1px 1px black;
+;
+}
+.cta {
+margin: 0 auto;
 }
 .nav-options {
   text-decoration: none;
+  color: #00e88a;
+  
 }
 
 
