700ce08cd7875a9793e499ba07ef82788da58a1a
Benjamin Grover <benmgrover@gmail.com>
2023-04-18 20:40:29 -0400

    backing splash comp and home view

--- vue/src/components/SplashPage.vue
+++ vue/src/components/SplashPage.vue
@@ -1,15 +1,47 @@
 <template>
-  <div>
-      This is the splash page for any user
+  <div id="wrapper" class="bg-image">
+    <main id="main-splash">
+      <h1>Battle the Monster of the Week!</h1>
+      <p></p>
+      <h2>Create your very own character to adventure with!</h2>
+      <b-button>Create your character</b-button>
+      <h2>Assemble your party!</h2>
+      <p>
+        The combination of characters that gets picked the most is the winning
+        party!
+      </p>
+      <p>
+        This needs an image, like the one in the public folder. This is the
+        splash page for any user
+      </p>
+    </main>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped>
+#wrapper {
+  background-color: #3a5268;
+  width: 100%;
+
+}
+.bg-image {
+    background-image: url("");
+
+}
+#main-splash {
+  width: 80%;
+  margin: 0 auto;
+}
+
+
+
+
+
+
+
 
 </style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="home">
-    <p id="intro">
+  <div id="home">
+    <splash-page />
+    <!-- <p id="intro">
       Welcome to Monster of the Week, where you pick your party to battle
       against a monster every week. Start by
       <b-btn class="callToAction">
@@ -14,7 +15,7 @@
           >Forming a Party</router-link
         >
       </b-btn>
-    </p>
+    </p> -->
     <div id="main-battle">
       <div class="monster-space">
         <monster-splash />
@@ -34,6 +35,7 @@
 import MonsterSplash from "../components/MonsterSplash.vue";
 import PartyComponent from "../components/PartyComponent.vue";
 import PartyWinner from "../components/PartyWinner.vue";
+import SplashPage from '../components/SplashPage.vue';
 import CharacterService from '../services/CharacterService';
 
 export default {
@@ -49,6 +51,7 @@ export default {
     MonsterSplash,
     PartyComponent,
     PartyWinner,
+    SplashPage,
   },
 
   methods: {
@@ -78,7 +81,7 @@ export default {
 
 <style scoped>
 
-.home {
+#home {
  /* display: grid;
  background: #096660;
  grid-template-areas:
